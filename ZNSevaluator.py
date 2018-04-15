ba1fa3ecf4a452b4c4e3bbd9ce10a5103b95934c
scorpiobg <nengchen11@fudan.edu.cn>
2018-04-15 18:30:17 -0400

    thread creation

--- ZNSevaluator.py
+++ ZNSevaluator.py
@@ -1,12 +1,14 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 import math
+import thread
+import time
 
-def devThread():
+
+def standardDevThread(key_pos, value_pos, H, T, query_no):
     def standDevH(data):
-        H = 1.0
-        if data.count():
-            key_val_cnt = data
+        key_val_cnt = data.filter(lambda x: x[1][1] > 0)
+        if key_val_cnt.count():
             keys_val_cnt = key_val_cnt.reduceByKey(lambda x, y: (x[0]+y[0], x[1]+y[1]))
             total_cnt = keys_val_cnt.map(lambda x: x[1][1]).reduce(lambda x, y: x+y)
             total_val = keys_val_cnt.map(lambda x: x[1][0]).reduce(lambda x, y: x+y)
@@ -20,21 +22,26 @@ def devThread():
                 devFromAverg = keys_averg.map(lambda x: (x[0], abs(x[1] - averg)/dev))
                 devH = devFromAverg.filter(lambda x: x[1] > H)
                 print "devH:", devH.collect()
-    T = 5
-    key_pos = 2 # src_pos = 2, protocal_pos = 4
-    value_pos = 5
-    words = data.map(lambda line: line.split(' ')).map(lambda record: (record[key_pos], float(record[value_pos])))
-    # words = words.reduceByKeyAndWindow(lambda x, y: x + y, lambda x, y: x - y, 10, 2)
-    processed_words = words.map(lambda x: (x[0], (x[1], 1)))
-    processed_words.foreachRDD(standDevH)
+    sc = SparkContext("local[2]", "zns")
+    ssc = StreamingContext(sc, 1)
+    data = ssc.textFileStream(r"file:///Users/ChenNeng/Desktop/2018_Spring/Large_Data_Stream/project/data")
+    ssc.checkpoint(r"file:///Users/ChenNeng/Desktop/2018_Spring/Large_Data_Stream/project/checkpoint%d"%query_no)
+    words = data.map(lambda line: line.split(' ')).map(lambda record: (record[key_pos], (float(record[value_pos]), 1)))
+    words.pprint()
+    processed_words = words.reduceByKeyAndWindow(lambda x, y: (x[0]+y[0], x[1]+y[1]), \
+        lambda x, y: (x[0]-y[0], x[1]-y[1]), T, 1)
+    words.foreachRDD(standDevH)
+    ssc.start()
+    ssc.awaitTermination()
+
+try:
+    thread.start_new_thread(standardDevThread, (2, 5, 1, 5, 1))
+except:
+   print "Error: unable to start thread"
+
+while 1:
+    pass
 
 
-
-sc = SparkContext("local[2]", "zns")
-ssc = StreamingContext(sc, 1)
-data = ssc.socketTextStream('localhost', 9999)
-devThread()
-ssc.start()
-ssc.awaitTermination()
 
 
