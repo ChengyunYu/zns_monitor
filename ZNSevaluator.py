d631805f58f6c9f8c310775318bde2d209d7e1be
scorpiobg <nengchen11@fudan.edu.cn>
2018-04-16 17:17:57 -0400

    finish 3 thread

--- ZNSevaluator.py
+++ ZNSevaluator.py
@@ -6,36 +6,73 @@ import time
 
 
 def standardDevThread(key_pos, value_pos, H, T, query_no):
-    def standDevH(data):
-        key_val_cnt = data.filter(lambda x: x[1][1] > 0)
+    def standDevX(data):
+        key_val_cnt = data.filter(lambda x: x[1] > 0)
         if key_val_cnt.count():
-            keys_val_cnt = key_val_cnt.reduceByKey(lambda x, y: (x[0]+y[0], x[1]+y[1]))
-            total_cnt = keys_val_cnt.map(lambda x: x[1][1]).reduce(lambda x, y: x+y)
-            total_val = keys_val_cnt.map(lambda x: x[1][0]).reduce(lambda x, y: x+y)
+            total_cnt = key_val_cnt.count()
+            total_val = key_val_cnt.map(lambda x: x[1]).reduce(lambda x, y: x+y)
             averg = total_val/total_cnt
-            print "total:", keys_val_cnt.collect()
+            print "total cnt:", total_cnt
+            print "total:", key_val_cnt.collect()
             print "average:", averg
-            keys_averg = keys_val_cnt.map(lambda x: (x[0], x[1][0]/x[1][1]))
-            dev = math.sqrt(keys_averg.map(lambda x: (x[1] - averg)**2).reduce(lambda x, y: x+y)/total_cnt)
+            dev = math.sqrt(key_val_cnt.map(lambda x: (x[1] - averg)**2).reduce(lambda x, y: x+y)/total_cnt)
             print "dev:", dev
             if dev:
-                devFromAverg = keys_averg.map(lambda x: (x[0], abs(x[1] - averg)/dev))
+                devFromAverg = key_val_cnt.map(lambda x: (x[0], abs(x[1] - averg)/dev))
                 devH = devFromAverg.filter(lambda x: x[1] > H)
                 print "devH:", devH.collect()
     sc = SparkContext("local[2]", "zns")
     ssc = StreamingContext(sc, 1)
     data = ssc.textFileStream(r"file:///Users/ChenNeng/Desktop/2018_Spring/Large_Data_Stream/project/data")
     ssc.checkpoint(r"file:///Users/ChenNeng/Desktop/2018_Spring/Large_Data_Stream/project/checkpoint%d"%query_no)
-    words = data.map(lambda line: line.split(' ')).map(lambda record: (record[key_pos], (float(record[value_pos]), 1)))
-    words.pprint()
-    processed_words = words.reduceByKeyAndWindow(lambda x, y: (x[0]+y[0], x[1]+y[1]), \
-        lambda x, y: (x[0]-y[0], x[1]-y[1]), T, 1)
-    words.foreachRDD(standDevH)
+    words = data.map(lambda line: line.split(' ')).map(lambda record: (record[key_pos], float(record[value_pos])))
+    processed_words = words.reduceByKeyAndWindow(lambda x, y: x+y, \
+        lambda x, y: x-y, T, 2)
+    processed_words.pprint()
+    processed_words.foreachRDD(standDevX)
+    ssc.start()
+    ssc.awaitTermination()
+
+def bandWidthThread(key_pos, value_pos, X, T, query_no):
+    def bandH(data):
+        key_val_cnt = data.filter(lambda x: x[1] > 0)
+        if key_val_cnt.count():
+            total_cnt = key_val_cnt.count()
+            total_val = key_val_cnt.map(lambda x: x[1]).reduce(lambda x, y: x+y)
+            moreThanX = key_val_cnt.map(lambda x: (x[0], x[1]/total_val)).filter(lambda x: x[1] > X)
+            print "bandH:", moreThanX.collect()
+    sc = SparkContext("local[2]", "zns")
+    ssc = StreamingContext(sc, 1)
+    data = ssc.textFileStream(r"file:///Users/ChenNeng/Desktop/2018_Spring/Large_Data_Stream/project/data")
+    ssc.checkpoint(r"file:///Users/ChenNeng/Desktop/2018_Spring/Large_Data_Stream/project/checkpoint%d"%query_no)
+    words = data.map(lambda line: line.split(' ')).map(lambda record: (record[key_pos], float(record[value_pos])))
+    processed_words = words.reduceByKeyAndWindow(lambda x, y: x+y, \
+        lambda x, y: x-y, T, 2)
+    processed_words.pprint()
+    processed_words.foreachRDD(bandH)
+    ssc.start()
+    ssc.awaitTermination()
+
+def topKThread(key_pos, value_pos, K, T, query_no):
+    def topK(data):
+        key_val_cnt = data.filter(lambda x: x[1] > 0)
+        if key_val_cnt.count():
+            topk = key_val_cnt.top(K, key=lambda x: x[1])
+            print "topk:", topk
+    sc = SparkContext("local[2]", "zns")
+    ssc = StreamingContext(sc, 1)
+    data = ssc.textFileStream(r"file:///Users/ChenNeng/Desktop/2018_Spring/Large_Data_Stream/project/data")
+    ssc.checkpoint(r"file:///Users/ChenNeng/Desktop/2018_Spring/Large_Data_Stream/project/checkpoint%d"%query_no)
+    words = data.map(lambda line: line.split(' ')).map(lambda record: (record[key_pos], float(record[value_pos])))
+    processed_words = words.reduceByKeyAndWindow(lambda x, y: x+y, \
+        lambda x, y: x-y, T, 2)
+    processed_words.pprint()
+    processed_words.foreachRDD(topK)
     ssc.start()
     ssc.awaitTermination()
 
 try:
-    thread.start_new_thread(standardDevThread, (2, 5, 1, 5, 1))
+    thread.start_new_thread(topKThread, (2, 5, 3, 6, 1))
 except:
    print "Error: unable to start thread"
 
