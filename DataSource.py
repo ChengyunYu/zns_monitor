f162f16e3070c23af672aab546ddfb61255d81d6
scorpiobg <nengchen11@fudan.edu.cn>
2018-04-16 17:59:01 -0400

    reverse the unexpected change

--- DataSource.py
+++ DataSource.py
@@ -7,19 +7,12 @@
 import socket
 import sys
 import numpy as np
-import time
-import random
-
 HOST = ''
-PORT = [9999, 10000]
-mysockets = []
-min_size, max_size = 1, 1000
-
+PORT = 9999
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
 try:
-    for p in PORT:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((HOST, p))
-        mysockets.append(s)
+    s.bind((HOST, PORT))
 except socket.error , msg:
     print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
     sys.exit()
@@ -27,6 +20,7 @@ print 'Socket bind complete'
 s.listen(10)
 print 'Socket now listening'
 conn, addr = s.accept()
+import numpy as np
 versions = ['ipv4', 'ipv6']
 protocols = ['HTTP', 'SMTP', 'TCP', 'UDP', 'SNMP', 'FTP', 'BGP', 'BGP', 'DHCP', 'SSH']
 ips = ['172.16.254.1', '255.255.255.128', '192.168.0.0', '172.16.0.0', '172.16.254.1', '255.255.128.0', '216.3.128.12', '24.60.91.16', '1.40.215.65', '23.129.64.104']
@@ -38,10 +32,6 @@ while 1:
     version = np.random.choice(versions, p = vfracs)
     protocol = np.random.choice(protocols, p = pfracs)
     sour, dest = np.random.choice(ips, 2, p = ifracs)
-    size = random.randint(min_size, max_size)
-    msg = str(idx) + ' ' + version + ' ' + sour + ' ' + dest + ' ' + protocol + ' ' + str(size) + '\n'
-    print msg
-    conn.send(msg)
+    size = np.random.random_sample()
+    conn.send(str(idx) + ' ' + version + ' ' + sour + ' ' + dest + ' ' + protocol + ' ' + str(size) + '\n')
     idx = idx + 1
-    time.sleep(0.25)
-
